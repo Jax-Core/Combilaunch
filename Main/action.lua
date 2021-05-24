75f1da9e73f1189a4be3d52e9ed9c965305ec2c5
Jax <80020581+EnhancedJax@users.noreply.github.com>
2021-05-24 21:26:09 +0800

    init

---
+++ Main/action.lua
@@ -0,0 +1,34 @@
+function Initialize()
+    local num = SELF:GetNumberOption("Number")
+    local sectionName = SELF:GetOption("SectionName")
+    local measureName = SKIN:GetMeasure("Launch") --//Name of script measure
+    
+
+    local file = io.open(SKIN:MakePathAbsolute(SELF:GetOption("IncFile")), "w")
+    
+    local t = {}
+
+    table.insert(t, "[Variables]")    
+
+    for i = 1, num do
+        local opt = SELF:GetOption("Option0")
+
+        if i == 1 then
+            table.insert(t, opt .. i .. "=" .. doSub(measureName:GetOption("IfMatchAction")))
+        else
+            table.insert(t, opt .. i .. "=" .. doSub(measureName:GetOption("IfMatchAction" .. i)))
+        end
+    end
+    
+    file:write(table.concat(t, "\n"))
+    file:close()
+end
+function doSub(value)
+    local substitution = SELF:GetOption("BlankSubstitution")
+    local a = string.match(value, '[?"](.+)[?"]')
+    if a==null then
+        return substitution
+    else
+        return a
+    end
+end
